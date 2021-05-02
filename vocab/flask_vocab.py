2c05a004f73105aba977848cce260f364805006e
James Taylor <solitarebecauseneon@gmail.com>
2021-05-01 21:11:45 -0700

    Update1: Implemented README specs in all files

--- vocab/flask_vocab.py
+++ vocab/flask_vocab.py
@@ -26,7 +26,7 @@ app.secret_key = CONFIG.SECRET_KEY  # Should allow using session variables
 # One shared 'Vocab' object, read-only after initialization,
 # shared by all threads and instances.  Otherwise we would have to
 # store it in the browser and transmit it on each request/response cycle,
-# or else read it from the file on each request/responce cycle,
+# or else read it from the file on each request/response cycle,
 # neither of which would be suitable for responding keystroke by keystroke.
 
 WORDS = Vocab(CONFIG.VOCAB)
@@ -74,7 +74,7 @@ def success():
 #   a JSON request handler
 #######################
 
-@app.route("/_check", methods=["POST"])
+@app.route("/_check")
 def check():
     """
     User has submitted the form with a word ('attempt')
@@ -89,14 +89,15 @@ def check():
     # The data we need, from form and from cookie
     text = flask.request.form["attempt"]
     jumble = flask.session["jumble"]
-    matches = flask.session.get("matches", [])  # Default to empty list
+    # matches = flask.session.get("matches", [])  # Default to empty list # No need
 
-    # Is it good?
-    in_jumble = LetterBag(jumble).contains(text)
-    matched = WORDS.has(text)
-
+    # Variables passed to vocab.html
+    in_jumble = LetterBag(jumble).contains(text)  # In the jumble?
+    matched = WORDS.has(text)                     # Does it match?
+    valid_len = len(text) <= 20                   # Length
+    rslt = {"matched": matched, "in_jumble": in_jumble, "valid_len": valid_len}
     # Respond appropriately
-    if matched and in_jumble and not (text in matches):
+    """if matched and in_jumble and not (text in matches):
         # Cool, they found a new word
         matches.append(text)
         flask.session["matches"] = matches
@@ -110,12 +111,9 @@ def check():
     else:
         app.logger.debug("This case shouldn't happen!")
         assert False  # Raises AssertionError
+    """
 
-    # Choose page:  Solved enough, or keep going?
-    if len(matches) >= flask.session["target_count"]:
-        return flask.redirect(flask.url_for("success"))
-    else:
-        return flask.redirect(flask.url_for("keep_going"))
+    return flask.jsonify(result=rslt)
 
 
 ###############
